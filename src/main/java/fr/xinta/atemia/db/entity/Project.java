19ed8f6738077c3cc9354e1195dd077043036609
flaburgan <flaburgan@geexxx.fr>
2013-04-08 00:41:26 +0200

    Add start and end dates to the project entity, add methods to change start and end date

--- src/main/java/fr/xinta/atemia/db/entity/Project.java
+++ src/main/java/fr/xinta/atemia/db/entity/Project.java
@@ -12,6 +12,10 @@ public class Project extends AbstractEntity {
     private String name;
     private String department;
     private int nbDaysSold;
+    private int startWeek;
+    private int startYear;
+    private int endWeek;
+    private int endYear;
     @OneToMany
     private List<Week> weeks;
     @ManyToMany
@@ -45,6 +49,22 @@ public class Project extends AbstractEntity {
     public void setNbDaysSold(int nbHoursSold) {
 	this.nbDaysSold = nbHoursSold;
     }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public int getEndYear() {
+        return endYear;
+    }
+
+    public int getStartWeek() {
+        return startWeek;
+    }
+
+    public int getEndWeek() {
+        return endWeek;
+    }
     
     public List<Week> getWeeks() {
 	return weeks;
@@ -54,24 +74,61 @@ public class Project extends AbstractEntity {
 	return workers;
     }
     
-    public void AddWorker(Person worker, int startWeek,
-            int startYear, int endWeek, int endYear) {
+    public void AddWorker(Person worker) {
         
 	workers.add(worker);
-//        int end = endWeek + endYear - startYear - 1;
-//	
-//	for (int i = startWeek; i <= end; i++) {
-//	    Week week = weeks.get(i);
-//	    if (week == null) {
-//		week = new Week();
-//		week.setNumber(i % 52);
-//                week.setYear(startYear + i / 52);
-//		weeks.add(i, week);
-//	    }
-//	    
-//            int[] tab = new int[Period.values().length - 1];
-//            tab[Period.PRODUCTION.ordinal()] = 5; //We put 5 days in production by default
-//	    week.getJob().add(workers.size() - 1, tab);
-//	}
+        for (Week week : weeks) {
+            int[] tab = new int[Period.values().length - 1];
+            tab[Period.PRODUCTION.ordinal()] = 5; //We put 5 days in production by default
+//            week.getJob().add(workers.size() - 1, tab);
+        }
+    }
+    
+    public void changeStartWeek(int newStartWeek, int newStartYear) {
+        
+        Week firstWeek = weeks.get(0);
+        int actualStartWeek = firstWeek.getNumber();
+        int actualStartYear = firstWeek.getYear();        
+        
+        // Add weeks while the previous start date is superior to the new start date
+        while ((actualStartYear == newStartYear && actualStartWeek > newStartWeek)
+                || actualStartYear > newStartYear) {
+            
+            if (actualStartWeek > 1) {
+                actualStartWeek--;
+            } else {
+                actualStartWeek = 52;
+                actualStartYear--;
+            }
+            
+            Week week = new Week();
+            week.setNumber(actualStartWeek);
+            week.setYear(actualStartYear);
+            weeks.add(0, week);
+        }
+    }
+    
+    public void changeEndWeek(int newEndWeek, int newEndYear) {
+        
+        Week lastWeek = weeks.get(weeks.size() - 1);
+        int actualEndWeek = lastWeek.getNumber();
+        int actualEndYear = lastWeek.getYear();        
+        
+        // Add weeks while the previous end date is inferior to the new end date
+        while ((actualEndYear == newEndYear && actualEndWeek < newEndWeek)
+                || actualEndYear < newEndYear) {
+            
+            Week week = new Week();
+            week.setNumber(actualEndWeek);
+            week.setYear(actualEndYear);
+            weeks.add(week);
+            
+            if (actualEndWeek < 52) {
+                actualEndWeek++;
+            } else {
+                actualEndWeek = 1;
+                actualEndYear++;
+            }
+        }
     }
 }
